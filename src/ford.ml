97a1e11ca4c1d68bb5b427a44abc2d14722748d6
HAWKDOTHACK <h4wk.hack@pm.me>
2025-12-28 11:56:17 +0100

    test commit4

--- src/ford.ml
+++ src/ford.ml
@@ -1,7 +1,7 @@
 open Graph
 open Tools
 
-(*COMMENTAIRE DE TEST3*)
+(*COMMENTAIRE DE TEST4*)
 
 let out_arc_not_null arc_list = List.fold_left (fun acu x -> if(x.lbl = 0) then acu else x::acu) [] arc_list
 
